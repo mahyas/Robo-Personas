0cbe401ec16a5f0b71dd1bab27675685622e560f
mahyas <mahyas@github.com>
2020-05-29 23:58:42 -0700

    added style

--- src/Card.js
+++ src/Card.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Card = ({id, name, email}) =>{
     return(
-        <div className='tc mw=10 bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+        <div className='tc mw=10 bg-washed-yellow dib br3 pa3 ma2 grow bw2 shadow-5'>
             <img alt= 'Robot' src= {`https://robohash.org/${id}`} width="200" height="200"></img>
             <div>
                 <h2>{name}</h2>
