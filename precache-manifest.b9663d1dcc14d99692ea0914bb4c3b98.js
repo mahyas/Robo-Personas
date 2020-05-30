ce785e8542f566cb0612c22ebc97f9dbe04c846e
mahyas <mahyas@github.com>
2020-05-30 00:20:55 -0700

    Updates

---
+++ precache-manifest.b9663d1dcc14d99692ea0914bb4c3b98.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63b40d5aea98060a3ce6fbeecb0a74d4",
+    "url": "/Robo-Personas/index.html"
+  },
+  {
+    "revision": "c2c83df876816cfe5cf1",
+    "url": "/Robo-Personas/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "ed6b4331841b6b650198",
+    "url": "/Robo-Personas/static/css/main.2abe4a92.chunk.css"
+  },
+  {
+    "revision": "c2c83df876816cfe5cf1",
+    "url": "/Robo-Personas/static/js/2.7a9ef991.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Robo-Personas/static/js/2.7a9ef991.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed6b4331841b6b650198",
+    "url": "/Robo-Personas/static/js/main.0e1af587.chunk.js"
+  },
+  {
+    "revision": "fdbde5940592dd4be16d",
+    "url": "/Robo-Personas/static/js/runtime-main.d3be6b24.js"
+  },
+  {
+    "revision": "0841af952c807bdf56455b1addb4c7df",
+    "url": "/Robo-Personas/static/media/IndieFlower-Regular.0841af95.ttf"
+  }
+]);
